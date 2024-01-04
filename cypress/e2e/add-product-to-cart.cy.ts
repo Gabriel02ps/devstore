10822e179add06cad2df75773c5502c09bf263dc
Gabriel02ps <gabriel02ps@gmail.com>
2024-01-04 02:51:37 -0300

    test: add search tests

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,9 @@
 describe('add product to cart', () => {
-  it('should be able to navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
+  it('should be able to navigate to the product page and add it to the cart', () => {
     cy.get('a[href^="/product/"]').first().click()
 
     cy.url().should('include', '/product')
@@ -11,8 +13,6 @@ describe('add product to cart', () => {
   })
 
   it('should not count duplicated products on cart', () => {
-    cy.visit('http://localhost:3000/')
-
     cy.get('a[href^="/product/"]').first().click()
 
     cy.url().should('include', '/product')
@@ -24,8 +24,6 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
-
     cy.get('input[name="q"]').type('Camiseta').parent().submit()
 
     cy.get('a[href^="/product/"]').first().click()
